fd86e5866071449e1731d01c34a4110daf0419d9
7computation <107147604+7computation@users.noreply.github.com>
2022-06-09 15:44:35 -0500

    Fixed to keep original path while adding new one

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express')
 var app = express()
 const port = 3000
 
+app.get('/', (request, result) => {
+	result.send(`Hello ${request.query.name || 'World'}!`)
+})
+
 app.get('/van', (request, result) => {
 	result.send('Hello from Van!')
 })
