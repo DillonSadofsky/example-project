5f0cb90a5bb03c75dc196bf6c9e3af72ab645161
rayzer <rayzerbrain@gmail.com>
2022-06-09 11:03:46 -0500

    chrome launch settings and name inserted

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express')
 var app = express()
 const port = 3000
 
-app.get('/', (request, result) => {
-	result.send(`Hello ${request.query.name || 'World'}!`)
+app.get('/ray', (request, result) => {
+	result.send(`Hello ${request.query.name}!`)
 })
 
 app.listen(port, () => {
