2ddf92a52ec6d7e6dcf431dc307a88909b359ddd
7computation <107147604+7computation@users.noreply.github.com>
2022-06-09 10:33:33 -0500

    Van's first commit.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express')
 var app = express()
 const port = 3000
 
-app.get('/', (request, result) => {
-	result.send(`Hello ${request.query.name || 'World'}!`)
+app.get('/van', (request, result) => {
+	result.send('Hello from Van!')
 })
 
 app.listen(port, () => {
