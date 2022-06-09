d2cd09d8dec2d0091fa7f9b519b8efe953a9c980
SaketR1 <sreddyj2023@gmail.com>
2022-06-09 13:41:15 -0500

    Completed homework

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var express = require('express')
 var app = express()
 const port = 3000
 
-app.get('/', (request, result) => {
-	result.send(`Hello ${request.query.name || 'World'}!`)
+app.get('/SaketReddy', (request, result) => {
+	//result.send(`Hello ${request.query.name || 'World'}!`)
+	result.send(`Hi Saket Reddy!`)
 })
 
 app.listen(port, () => {
