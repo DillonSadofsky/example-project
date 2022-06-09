3ca68b8daac35d32ad6bf939eb1ccd0627b77bfd
Izak-Kriegbaum <107147658+Izak-Kriegbaum@users.noreply.github.com>
2022-06-09 13:25:35 -0500

    Update index.js
    My route

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (request, result) => {
 	result.send(`Hello ${request.query.name || 'World'}!`)
 })
 
+app.get('/Izak', (request, result) => {
+	// I wanted this to be a hyperlink in my text but I'm not
+	// sure how to do this, I would love to learn.
+	var mySite = ('http://yellowiron.flywheelsites.com/izak/')
+
+	result.send('Greetings, here is my webpage: ' +  mySite)
+})
+
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
 })
