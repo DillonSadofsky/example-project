9d0fb8d68ff831df7838c59cee3362bdbb224a7c
elsamatics507 <emeyer@isoftdata.com>
2022-06-13 12:18:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.get('/ray', (request, result) => {
 	result.send(`Hello! (Connecting IP: ${request.ip || "Error, IP could not be found"})`)
 })
 
+app.get('/elsa', (request, result) => {
+	result.send(`Heyya :3. I see u`)
+})
+
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
 })
